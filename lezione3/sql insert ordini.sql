c352ef13026269de6d6d15fac42710f2aeff4ebd
soluzione-software <info@soluzionesoftware.com>
2025-02-28 15:08:08 +0100

    sql 4

---
+++ lezione3/sql insert ordini.sql
@@ -0,0 +1,5 @@
+#INSERT almeno 5 ordini 2 per il cliente Davide e 3 per il cliente Mattia
+#ovviamente inserire i record nella tabella ordini e dettaglio_ordini
+
+# 10 insert se 1 prodotto ogni ordine
+#15 insert se 2 prodotti ogni ordine
--- lezione3/sql where.sql
+++ lezione3/sql where.sql
@@ -1,3 +1,4 @@
+/*
 #query con =
 select cliente_id, cognome, nome from clienti where nome = "Mattia";
 
@@ -34,5 +35,16 @@ select cognome,nome,cliente_id from clienti where cognome LIKE "%TIA%";
 
 #query OPERATORI LOGICI 
 #AND
+#tutti i fiori che hanno un nome che inizia con T e un prezzo < a 4€
+select nome from fiori where nome LIKE "T%" and prezzo < 4;
+
 #OR
+#costruire una query a piacere con clausola OR
+select * from clienti where nome = "Francesco" or cognome = "Rossi";
+
 #query con NOT
+#costruire una query a piacere con clausola NOT
+select * from fiori where NOT prezzo > 5;
+*/
+#prezzo è ivato, voglio scorporare l'iva
+select nome, prezzo / 1.22 as prezzo_imponibile from fiori;
